762c54560f5120ea95419711faf83d0665b2e2e0
dfahlander <david.fahlander@gmail.com>
2020-09-14 01:32:01 +0200

    Universal JS support

--- libs/dreambase-library/src/typeson-simplified/presets/builtin.ts
+++ libs/dreambase-library/src/typeson-simplified/presets/builtin.ts
@@ -3,8 +3,8 @@ import SpecialNumberDef from "../types/SpecialNumber.js";
 import DateDef from "../types/Date.js";
 import SetDef from "../types/Set.js";
 import MapDef from "../types/Map.js";
-import TypedArraysDefs from "../types/TypedArray";
-import ArrayBufferDef from "../types/ArrayBuffer";
+import TypedArraysDefs from "../types/TypedArray.js";
+import ArrayBufferDef from "../types/ArrayBuffer.js";
 
 export default {
   ...bigintDef,
