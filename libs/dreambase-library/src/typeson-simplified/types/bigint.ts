1a3eb3902ddfb9ba0fc6d1bf6cbc4f6e52ba67ee
dfahlander <david.fahlander@gmail.com>
2021-05-28 16:46:23 +0200

    Represent positive bigints in 64bit number-strings

--- libs/dreambase-library/src/typeson-simplified/types/bigint.ts
+++ libs/dreambase-library/src/typeson-simplified/types/bigint.ts
@@ -1,24 +1,31 @@
 import { b64ToBigInt, bigint2B64 } from "../../common/bigint-conversion.js";
 import { TypeDefSet } from "../TypeDefSet.js";
 
+export const MAXUINT64 =
+  typeof BigInt !== "undefined"
+    ? BigInt("18446744073709551615")
+    : 0xffff_ffff_ffff;
+
 const bigIntDef: TypeDefSet = {
   bigint: {
     replace: (realVal: bigint) => {
-      const negative = realVal < 0;
-      const b64 = bigint2B64(negative ? -realVal : realVal);
-      return negative
-        ? {
-            $t: "bigint",
-            neg: true,
-            b64,
-          }
-        : {
-            $t: "bigint",
-            b64,
-          };
+      if (realVal <= MAXUINT64) {
+        // Negative and realtively small numbers - represent as normal numbers
+        return { $t: "bigint", v: "" + realVal };
+      } else {
+        // Very large positive numbers - compress to base64
+        const b64 = bigint2B64(realVal);
+        return {
+          $t: "bigint",
+          b64,
+        };
+      }
     },
-    revive: ({ neg, b64 }: { $t: "bigint"; neg?: boolean; b64: string }) =>
-      neg ? -b64ToBigInt(b64) : b64ToBigInt(b64),
+    revive: (
+      obj:
+        | { $t: "bigint"; v: string }
+        | { $t: "bigint"; neg?: boolean; b64: string }
+    ) => ("v" in obj ? BigInt(obj.v) : b64ToBigInt(obj.b64)),
   },
 };
 
