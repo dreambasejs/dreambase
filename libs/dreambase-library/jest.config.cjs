ffe2310cdae69aaaca422c30081e8907186472a6
David Fahlander <david.fahlander@gmail.com>
2026-02-04 09:03:36 +0100

    Fixed tests to run again

---
+++ libs/dreambase-library/jest.config.cjs
@@ -0,0 +1,8 @@
+const { defaults } = require("jest-config");
+
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  testRegex: "(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleNameMapper: { ...defaults.moduleNameMapper, "(.*)\\.js$": "$1" },
+};
